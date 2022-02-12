303703eeead6f0893ee2e7dc1920ca5f7d964e4c
Nihar Gupte <nihar71298@gmail.com>
2022-02-12 16:34:07 +0530

    Task 1.2 Progress

--- brieflySift/code/features.py
+++ brieflySift/code/features.py
@@ -25,16 +25,17 @@ def sift_descriptor(source, source_keypoint, target, target_keypoint, n=50):
 
 
 def brief_descriptor(source, source_keypoint, target, target_keypoint, n=50):
-    orb = cv2.ORB_create(n)
+    orb = cv2.xfeatures2d.BriefDescriptorExtractor_create()
+    print(len(source_keypoint))
     source_descriptor = orb.compute(source, source_keypoint)[1]
     target_descriptor = orb.compute(target, target_keypoint)[1]
     return source_descriptor, target_descriptor
 
 
-def brute_force(source, source_keypoint, source_descriptor, target, target_keypoint, target_descriptor):
+def brute_force(source, source_keypoint, source_descriptor, target, target_keypoint, target_descriptor, nm):
     brute_force_obj = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
     num_matches = brute_force_obj.match(source_descriptor, target_descriptor)
-    num_matches = sorted(num_matches, key=lambda x: x.distance)
+    num_matches = sorted(num_matches, key=lambda x: x.distance)[0:nm]
     output_image = cv2.drawMatches(source, source_keypoint, target, target_keypoint, num_matches, None, flags=2)
     return output_image
 
@@ -54,6 +55,8 @@ if __name__ == "__main__":
         "brief": brief_descriptor
     }
 
+    n_points = [300, 1000]
+
     parser.add_argument("-kp", "--kp", help='''Should be either of "fast" or "dog" ''', required=True)
     parser.add_argument("-des", "--des", help='''Should be either of "sift" or "brief" ''', required=True)
     parser.add_argument("-trans", "--trans", help='''Should be one of "view","scale","rot" or "light" ''',
@@ -65,6 +68,8 @@ if __name__ == "__main__":
     if not CLI_flag:
         print(CLI_flag)
         raise Exception("Invalid Command arguments : Command line arguments do not match the description. Enter again")
+    if args.nm > 300:
+        raise Exception("Number of matches cannot be more than 300 since the keypoints are 300 and 1000 in two iterations")
 
     source_img_name = args.trans + "_S.ppm"
     target_img_name = args.trans + "_T.ppm"
@@ -73,19 +78,20 @@ if __name__ == "__main__":
     S = cv2.imread(os.path.join(r"..\data", source_img_name))
     T = cv2.imread(os.path.join(r"..\data", target_img_name))
 
-    kp1, kp2 = function_call_dict[args.kp](S, T, N)
-    des1, des2 = function_call_dict[args.des](S, kp1, T, kp2, N)
+    for np in n_points:
+        kp1, kp2 = function_call_dict[args.kp](S, T, np)
+        des1, des2 = function_call_dict[args.des](S, kp1, T, kp2, np)
 
-    output = brute_force(S, kp1, des1, T, kp2, des2)
+        output = brute_force(S, kp1, des1, T, kp2, des2, N)
 
-    cv2.imshow("Output (Press q to quit)", output)
-    k = cv2.waitKey(0)
+        cv2.imshow("Output (Press q to quit)", output)
+        k = cv2.waitKey(0)
 
-    while True:
-        if k == ord('q'):
-            break
-    cv2.destroyAllWindows()
+        while True:
+            if k == ord('q'):
+                break
+        cv2.destroyAllWindows()
 
-    output_name = args.kp + "_" + args.des + "_" + args.trans + "_" + str(args.nm) + ".png"
-    output_path = os.path.join(r"..\results", output_name)
-    cv2.imwrite(output_path, output)
+        output_name = args.kp + "_" + args.des + "_" + args.trans + "_" + str(args.nm) + ".png"
+        output_path = os.path.join(r"..\results", output_name)
+        cv2.imwrite(output_path, output)
